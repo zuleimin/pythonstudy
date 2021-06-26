9cb85af373584d1fb38f10fdf1074472ea27cfbf
zuleimin <zuleimin@qq.com>
2021-06-26 20:46:52 +0800

    basic

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # pythonstudy
+https://www.py.cn/jishu/jichu/13184.html
+http://www.py.cn/code/17774.html
---
+++ basic.py
@@ -0,0 +1,10 @@
+import os
+import time
+import datetime
+this_year=2021
+myname=input('pls input your name:')
+myage=int(input('how old are you:'))
+
+print('hello ' ,myname + '\n')
+print('you are ',myage, 'years old')
+print('so you were born in : ', this_year-myage)
